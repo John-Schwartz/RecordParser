5db2e553866c537798826365befbacdce2be52c5
john.schwartz <john.schwartz@EC2AMAZ-5B6B9MP>
2019-05-20 10:33:37 -0600

    Beginning RESTAPI setup, added Records controller, model, view

---
+++ RESTfulAPI/Controllers/RecordsController.cs
@@ -0,0 +1,47 @@
+﻿using RESTfulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RESTfulAPI.Controllers
+{
+
+    public class RecordsController : ApiController
+    {
+
+        // GET api/values
+        public IEnumerable<Record> Get()
+        {
+            return new List<Record> { };
+        }
+
+        // GET api/values/5
+        public string Get(string searchWord)
+        {
+            switch (searchWord)
+            {
+                case "gender":
+                    var genderSorted = "";
+                    break;
+
+                case "birthdate":
+                    var dobSorted = "";
+                    break;
+
+                case "name":
+                    var nameSorted = "";
+                    break;
+            }
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]Record record)
+        {
+            var recordString = $"{record.LastName}, {record.FirstName}, {record.Gender}, {record.FavoriteColor}, {record.DateOfBirth.ToString("M/d/yyyy")}";
+        }
+    }
+}
---
+++ RESTfulAPI/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTfulAPI.Models
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Gender { get; set; }
+        public string FavoriteColor { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
